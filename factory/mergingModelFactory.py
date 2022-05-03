6317aaac97f30148b6d7ad9de84855ff07a69c8f
unknown <ajcltm@gmail.cmo>
2022-05-03 20:34:23 +0900

    2022. 5.3. 22:34

--- factory/mergingModelFactory.py
+++ factory/mergingModelFactory.py
@@ -1,73 +1,114 @@
 import sys
 parentPath='c:/Users/user/PycharmProjects/personnelInfo' # parent 경로
 sys.path.append(parentPath) # 경로 추가
+from abc import ABC, abstractclassmethod
 import pandas as pd
 from datetime import datetime
 from config import config
 from util import util
 from factory import simpleModelFactory, advancedModelFactory
 
-class DbAtTime:
-    def __init__(self, date):
+class IIdModels(ABC):
+
+    @abstractclassmethod
+    def get_df(self):
+        pass
+
+    @abstractclassmethod
+    def merge(self):
+        pass
+
+
+class IdModels(IIdModels):
+
+    def __init__(self, base_df, dfs_list):
+        self.base_df = base_df
+        self.dfs_list = dfs_list
+
+    def get_df(self):
+        self.df = self.merge()
+        return self.df
+
+    def merge(self):
+        df = self.base_df
+        for i in self.dfs_list:
+            df = pd.merge(df, i, how='left', on='id_')
+        return df
+
+class EmployeeListFactory:
+
+    def __init__(self, date) :
         self.date=date
-        self.dfs = []   
-        self.compose_df()
-        self.df = self.get_df()
-    
-    def register_df(self, df):
-        self.dfs.append(df)
-    
-    def compose_df(self):
+
+    def get_df(self) -> IIdModels:
+        base_df = self.get_base_df()
+        df1 = advancedModelFactory.DepartmentPositionLevelDf(self.date).df
+        df2 = advancedModelFactory.ContactState(self.date).df
+        dfs_list = [df1, df2]
+        df = IdModels(base_df, dfs_list).get_df()
+        df = self.process(df)
+        return df
+
+    def get_base_df(self):
         df = simpleModelFactory.UniqueInfoDf().df
         con1 = df.loc[:, 'resignationDate'] >= self.date
         con2 = df.loc[:, 'resignationDate'].isna()
         con3 = df.loc[:, 'recruitmentDate'] <= self.date
         df = df.loc[(con1|con2) & con3]
-        self.register_df(df)
-        self.register_df(advancedModelFactory.DepartmentPositionLevelDf(self.date).df)
-        self.register_df(advancedModelFactory.ContactState(self.date).df)
-        self.register_df(simpleModelFactory.ContactInfoDf().df)
+        return df
 
-    def get_df(self):
-        df = self.dfs[0]
-        for i in self.dfs[1:]:
-            df = pd.merge(df, i, how='left', on='id_')
+    def process(self, df):
         df=df.sort_values(by=['recruitmentDate', 'birth'])
         df=df.sort_values(by=['appoPosLevel_to_today'], ascending=False)
         df=df.sort_values(by=['depart_order', 'level_order', 'appoLeader'])
         df = df.reset_index()
         return df
 
-class Resignation:
-    def __init__(self, date):
+class ResignationFactory:
+
+    def __init__(self, date) :
         self.date=date
-        self.dfs = []   
-        self.compose_df()
-        self.df = self.get_df()
-    
-    def register_df(self, df):
-        self.dfs.append(df)
-    
-    def compose_df(self):
+
+    def get_df(self) -> IIdModels:
+        base_df = self.get_base_df()
+        df1 = advancedModelFactory.DepartmentPositionLevelDf(self.date).df
+        df2 = advancedModelFactory.ContactState(self.date).df
+        dfs_list = [df1, df2]
+        df = IdModels(base_df, dfs_list).get_df()
+        df = self.process(df)
+        return df
+
+    def get_base_df(self):
         df = simpleModelFactory.UniqueInfoDf().df
         con1 = df.loc[:, 'resignationDate'] <= self.date
         df = df.loc[(con1)]
-        self.register_df(df)
-        self.register_df(advancedModelFactory.DepartmentPositionLevelDf(self.date).df)
-        self.register_df(advancedModelFactory.ContactState(self.date).df)
+        return df
 
-    def get_df(self):
-        df = self.dfs[0]
-        for i in self.dfs[1:]:
-            df = pd.merge(df, i, how='left', on='id_')
+    def process(self, df):
         df=df.sort_values(by=['recruitmentDate', 'birth'])
         df=df.sort_values(by=['appoPosLevel_to_today'], ascending=False)
         df=df.sort_values(by=['depart_order', 'level_order', 'appoLeader'])
         df = df.reset_index(drop=True)
         return df
 
-date = datetime(2022,4,27)
-df = DbAtTime(date).df
+class DataProvider:
+    __dict = {
+        '사원명부' : EmployeeListFactory,
+        '퇴직명부' : ResignationFactory
+    }
+    
+    def get_data(self, data, date):
+        __callable = self.__dict.get(data)
+        df = __callable(date).get_df()
+        return df
 
-df.to_csv('DbAtTime.csv', encoding='cp949')
+
+
+date = datetime(2022,4,27)
+# df = DbAtTime(date).df
+
+# df.to_csv('DbAtTime.csv', encoding='cp949')
+# print(df)
+
+df = DataProvider().get_data('퇴직명부', date)
 print(df)
